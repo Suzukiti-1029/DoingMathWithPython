ac009157d7a891dfc0e5a9a88e3d8a99cacd8ead
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-02-06 15:21:24 +0900

    例外と不当入力の処理

---
+++ chapter1/1-4-1.py
@@ -0,0 +1,19 @@
+try:
+  a = float(input('Enter a number: '))
+except ValueError:
+  print('You entered an invalid number')
+3/4
+
+a = input('Input an integer: ')
+1
+
+a = int(input())
+1
+a + 1
+
+a = int(input())
+1.0
+
+1.1.is_integer()
+
+1.0.is_integer()

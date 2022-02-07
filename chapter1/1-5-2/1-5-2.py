f0ab896a058327fbc05eef1be8c450085c09d103
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-02-07 11:27:49 +0900

    乗算表を生成する

---
+++ chapter1/1-5-2/1-5-2.py
@@ -0,0 +1,15 @@
+item1 = 'apples'
+item2 = 'bananas'
+item3 = 'grapes'
+print('At the grocery store, I bought some {0} and {1} and {2}'.format(item1, item2, item3))
+
+print('Number 1: {0} Number 2: {1} '.format(1, 3.578))
+
+# script2.py
+
+'{0}'.format(1.25456)
+'{0:.2f}'.format(1.25456)
+
+'{0:.2f}'.format(1.25556)
+
+'{0:.2f}'.format(1)

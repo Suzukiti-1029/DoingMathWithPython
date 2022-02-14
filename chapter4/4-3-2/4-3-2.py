bb2cd030405fa976bc75571b5d757a385592d4cf
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-02-15 05:38:37 +0900

    1変数を他の変数について解く

---
+++ chapter4/4-3-2/4-3-2.py
@@ -0,0 +1,18 @@
+from sympy import Symbol, solve
+#
+x = Symbol('x')
+a = Symbol('a')
+b = Symbol('b')
+c = Symbol('c')
+
+expr = a*x*x + b*x + c
+solve(expr, x, dict=True)
+
+from sympy import Symbol, solve, pprint
+s = Symbol('s')
+u = Symbol('u')
+t = Symbol('t')
+a = Symbol('a')
+expr = u*t + (1/2)*a*t*t - s
+t_expr = solve(expr, t, dict=True)
+pprint(t_expr)
